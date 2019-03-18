17389d908e2491537bc913477a5ebeb8276d98d2
Martin <hackfin@section5.ch>
2019-03-18 13:17:31 +0100

    First amendments and scripts for auto testing

---
+++ docker/build_test.sh
@@ -0,0 +1,18 @@
+GOLDEN_REFERENCE=75dbbd81c839981b53fcb8998496af23ef35b11a
+
+GIT_REPO=https://github.com/hackfin/nhwcodec.git
+
+cd src && \
+[ -e nhw_reference ] || \
+	git clone $GIT_REPO nhw_reference
+cd src/nhw_reference
+git checkout restruct
+git checkout $GOLDEN_REFERENCE
+# Pick TEST suite from latest:
+git checkout restruct -- test
+
+cd $HOME/src/nhwcodec; make clean all
+cd $HOME/src/nhw_reference; make clean all
+
+cd $HOME/src/nhw_reference/test; make clean all-nhw reference
+cd $HOME/src/nhwcodec/test; make clean all-test all-diff
--- docker/init.sh
+++ docker/init.sh
@@ -6,17 +6,22 @@
 GIT_URL=https://github.com/hackfin/nhwcodec.git
 GIT_BRANCH=restruct
 
+pushd .
 # Raphael Canuts stable branch
 # GIT_URL=https://github.com/rcanut/nhwcodec.git
 # GIT_BRANCH=master
 
-cd $HOME/src && git clone $GIT_URL -b $GIT_BRANCH
+[ -e $HOME/src ] || mkdir $HOME/src
+cd $HOME/src; [ -e nhwcodec ] || git clone $GIT_URL -b $GIT_BRANCH
 
 WORKDIR=$HOME/work
 
 # Get images
-cd $WORKDIR && wget https://section5.ch/downloads/nhw_testimages.tgz && \
-	tar xfz nhw_testimages.tgz
+[ -e $WORKDIR ] || mkdir $WORKDIR
+[ -e $WORKDIR/nhw_testimages.tgz ] || \
+wget -o $WORKDIR/imgs.tgz https://section5.ch/downloads/nhw_testimages.tgz && \
+	cd $WORKDIR && tar xvfz nhw_testimages.tgz
 
 [ -e $WORKDIR/nhw/out ] || mkdir $WORKDIR/nhw/out
 [ -e $WORKDIR/nhw/ref ] || mkdir $WORKDIR/nhw/ref
+popd
